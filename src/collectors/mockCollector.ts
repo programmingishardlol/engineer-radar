ab65bec5eec18d73db67b88dc458ca7bf2a768a3
MarkS <mshen0@berkeley.edu>
2026-04-27 20:53:09 -0700

    Collector normalization mock pipeline

--- src/collectors/mockCollector.ts
+++ src/collectors/mockCollector.ts
@@ -2,5 +2,11 @@ import { mockRawItems } from "../mocks/rawItems";
 import type { RawItem } from "../types";
 
 export async function collectMockRawItems(): Promise<RawItem[]> {
-  return mockRawItems;
+  return mockRawItems.map((item) => ({
+    ...item,
+    metadata: {
+      ...item.metadata,
+      demo: true
+    }
+  }));
 }
